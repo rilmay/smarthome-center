a8408d87f7a47ea55b29c19b57d06c8e224bbda7
reeelmay <nikita.1999.slipknot@gmail.com>
2021-10-16 14:17:29 +0300

    initial commit

---
+++ src/main/java/com/guzov/smarthomecenter/SmarthomeCenterApplication.java
@@ -0,0 +1,13 @@
+package com.guzov.smarthomecenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmarthomeCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmarthomeCenterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/guzov/smarthomecenter/SmarthomeCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guzov.smarthomecenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmarthomeCenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
